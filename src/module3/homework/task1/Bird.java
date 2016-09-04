280e18093f4dda13add202292f45b4a4e02fa36f
Olifer1 <oleh.olifer@gmail.com>
2016-09-04 22:53:06 +0300

    Homework. Module 3. Task 3.1

---
+++ src/module3/homework/task1/Bird.java
@@ -0,0 +1,14 @@
+package module3.homework.task1;
+
+public class Bird extends Animal{
+
+    void fly() {
+            System.out.println("I'm flying");
+    }
+    void sing() {
+        System.out.println("I'm singing");
+    }
+    void name() {
+        System.out.println("I'm Bird");
+    }
+}
---
+++ src/module3/homework/task1/Solution.java
@@ -0,0 +1,13 @@
+package module3.homework.task1;
+
+
+public class Solution {
+    public static void main(String[] args){
+
+        Bird Sparrow = new Bird();
+        Sparrow.walk();
+        Sparrow.fly();
+        Sparrow.sing();
+        Sparrow.name();
+    }
+}
